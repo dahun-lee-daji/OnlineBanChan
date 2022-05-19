f5e10a237c4838f1df127459a032db97ded75ad6
dahun-lee-daji <ssss6596@gmail.com>
2022-05-19 18:17:42 +0900

    init: proj. 생성

---
+++ OnlineBanChan/OnlineBanChan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineBanChan
+//
+//  Created by 이다훈 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

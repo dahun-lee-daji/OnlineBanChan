21b60c1538b5adfb41f431aa0efe84ef2cd9427c
dahun-lee-daji <ssss6596@gmail.com>
2022-05-28 21:27:03 +0900

    feat: detailFoodViewController 디자인 일부분 구현 #6

---
+++ OnlineBanChan/OnlineBanChan/Presentation/DetailView/DetailFoodViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailFoodViewController.swift
+//  OnlineBanChan
+//
+//  Created by 이다훈 on 2022/05/28.
+//
+
+import UIKit
+
+class DetailFoodViewController: UIViewController, StoryboardInitiating {
+    
+}

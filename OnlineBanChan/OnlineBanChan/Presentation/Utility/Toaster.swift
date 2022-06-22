335a438d88aaab2785aae747538eab804c70bdcf
dahun-lee-daji <ssss6596@gmail.com>
2022-06-22 15:54:12 +0900

    Feat: Toast메세지가 여러개 출력되지 않도록 구현 #5

--- OnlineBanChan/OnlineBanChan/Presentation/Utility/Toaster.swift
+++ OnlineBanChan/OnlineBanChan/Presentation/Utility/Toaster.swift
@@ -10,33 +10,53 @@ import RxSwift
 
 class Toaster {
     
+    class ToasterLabel: UILabel {
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setConfigure()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            setConfigure()
+        }
+        
+        private func setConfigure() {
+            self.numberOfLines = 0
+            self.translatesAutoresizingMaskIntoConstraints = false
+            self.backgroundColor = .systemGray2
+            self.layer.cornerRadius = 10
+            self.clipsToBounds = true
+            self.font = UIFont.boldSystemFont(ofSize: 20)
+            
+            let gesture = UITapGestureRecognizer.init(target: self, action: #selector(self.removeFromSuperview))
+            self.addGestureRecognizer(gesture)
+            
+            UIView.animate(withDuration: 4.0, delay: 0.1 ,animations: {
+                self.alpha = 0
+            }, completion: {
+                if $0 == true {
+                    self.removeFromSuperview()
+                }
+            })
+        }
+    }
+    
     func showUpWith(text: String, to view: UIView) {
-        let label = makeToasterLabel(text: text)
+        let label = ToasterLabel.init()
+        label.text = text
+        
+        if view.subviews.contains(where: {
+            $0 is ToasterLabel
+        }) {
+            return
+        }
         
         view.addSubview(label)
         
         label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
 
         label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.bounds.height * 0.7).isActive = true
-    }
-    
-    private func makeToasterLabel(text: String) -> UILabel {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .systemGray2
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        label.text = text
-        
-        UIView.animate(withDuration: 4.0, delay: 0.1 ,animations: {
-            label.alpha = 0
-        }, completion: {
-            if $0 == true {
-                label.removeFromSuperview()
-            }
-        })
-        
-        return label
     }
 }
